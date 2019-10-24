bc47f94c740b461ef6d6b8c722eede282a962fba
Romeo Orsolino <orso.romeo@gmail.com>
2019-10-24 10:33:25 +0200

    commented ros warnings on an older version of the library

--- src/rviz_visual_tools.cpp
+++ src/rviz_visual_tools.cpp
@@ -875,26 +875,26 @@ bool RvizVisualTools::trigger()
 
 bool RvizVisualTools::publishMarkers(visualization_msgs::MarkerArray& markers)
 {
-  if (pub_rviz_markers_ == nullptr)
-  {  // always check this before publishing
-    loadMarkerPub();
-  }
-
-  // Check if connected to a subscriber
-  if (!pub_rviz_markers_waited_ && !pub_rviz_markers_connected_)
-  {
-    ROS_DEBUG_STREAM_NAMED(name_, "Waiting for subscribers before publishing markers...");
-    waitForSubscriber(pub_rviz_markers_);
-
-    // Only wait for the publisher once, after that just ignore the lack of connection
-    pub_rviz_markers_waited_ = true;
-  }
-
-  // Check if any actual markers exist to publish
-  if (markers.markers.empty())
-  {
-    return false;
-  }
+//  if (pub_rviz_markers_ == nullptr)
+//  {  // always check this before publishing
+//    loadMarkerPub();
+//  }
+//
+//  // Check if connected to a subscriber
+//  if (!pub_rviz_markers_waited_ && !pub_rviz_markers_connected_)
+//  {
+//    ROS_DEBUG_STREAM_NAMED(name_, "Waiting for subscribers before publishing markers...");
+//    waitForSubscriber(pub_rviz_markers_);
+//
+//    // Only wait for the publisher once, after that just ignore the lack of connection
+//    pub_rviz_markers_waited_ = true;
+//  }
+//
+//  // Check if any actual markers exist to publish
+//  if (markers.markers.empty())
+//  {
+//    return false;
+//  }
 
   // Change all markers to be inverted in color
   if (psychedelic_mode_)
